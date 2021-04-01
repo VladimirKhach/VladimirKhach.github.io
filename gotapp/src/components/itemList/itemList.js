f64962f9dbb853ed222498c48b1d6c0c170d6968
Vladimir <59541286+VladimirKhach@users.noreply.github.com>
2021-04-01 17:20:39 +0300

    Add files via upload

---
+++ gotapp/src/components/itemList/itemList.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import './itemList.css';
+import Spinner from '../spinner';
+import PropTypes from 'prop-types';
+
+export default class ItemList extends Component {
+
+    state = {
+        itemList: null
+    }
+    static defaultProps = {
+        onItemSelected: () => {}
+    }
+    
+    static propTypes = {
+        onItemSelected: PropTypes.func
+    }
+
+    componentDidMount() {
+        const {getData} = this.props;
+
+        getData()
+            .then( (itemList) => {
+                this.setState({
+                    itemList
+                })
+            })
+    }
+
+    renderItems(arr) {
+        return arr.map((item) => {
+            const {id} = item;
+
+            const label = this.props.renderItem(item);
+
+            return (
+                <li 
+                    key={id}
+                    className="list-group-item"
+                    onClick={ () => this.props.onItemSelected(id)}>
+                    {label}
+                </li>
+            )
+        })
+    }
+
+    render() {
+        const {itemList} = this.state;
+
+        if (!itemList) {
+            return <Spinner/>
+        }
+
+        const items = this.renderItems(itemList);
+
+
+        return (
+            <ul className="item-list list-group">
+                {items}
+            </ul>
+        );
+    }
+}
---
+++ gotapp/src/components/randomChar/randomChar.js
@@ -0,0 +1,98 @@
+import React, {Component} from 'react';
+import './randomChar.css';
+import gotService from '../../services/gotService';
+import Spinner from '../spinner';
+import ErrorMessage from '../errorMessage';
+import PropTypes from 'prop-types';
+
+export default class RandomChar extends Component {
+
+    gotService = new gotService();
+    state = {
+        char: {},
+        loading: true,
+        error: false
+    }
+    
+
+    componentDidMount() {
+        this.updateChar();
+        this.timerId = setInterval(this.updateChar, this.props.interval);
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.timerId);
+    }
+
+    onCharLoaded = (char) => {
+        this.setState({
+            char,
+            loading: false
+        })
+    }
+
+    onError = (err) => {
+        this.setState({
+            error: true,
+            loading: false
+        })
+    }
+
+    updateChar = () => {
+        const id = Math.floor(Math.random()*140 + 25); //25-140
+        this.gotService.getCharacter(id)
+            .then(this.onCharLoaded)
+            .catch(this.onError);
+    }
+
+    render() {
+        const { char, loading, error } = this.state;
+
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error) ? <View char={char}/> : null;
+
+        return (
+            <div className="random-block rounded">
+                {errorMessage}
+                {spinner}
+                {content}
+            </div>
+        );
+    }
+}
+
+RandomChar.defaultProps = {
+    interval: 15000
+}
+
+RandomChar.propTypes = {
+    interval: PropTypes.number
+}
+
+const View = ({char}) => {
+    const {name, gender, born, died, culture} = char;
+    return (
+        <>
+            <h4>Random Character: {name}</h4>
+            <ul className="list-group list-group-flush">
+                <li className="list-group-item d-flex justify-content-between">
+                    <span className="term">Gender </span>
+                    <span>{gender}</span>
+                </li>
+                <li className="list-group-item d-flex justify-content-between">
+                    <span className="term">Born </span>
+                    <span>{born}</span>
+                </li>
+                <li className="list-group-item d-flex justify-content-between">
+                    <span className="term">Died </span>
+                    <span>{died}</span>
+                </li>
+                <li className="list-group-item d-flex justify-content-between">
+                    <span className="term">Culture </span>
+                    <span>{culture}</span>
+                </li>
+            </ul>
+        </>
+    )
+}
