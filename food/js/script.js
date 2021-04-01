5758da50ba9225ce74f49052658de514e7ec0c9b
Vladimir <59541286+VladimirKhach@users.noreply.github.com>
2021-04-01 15:02:16 +0300

    Add files via upload

---
+++ food/js/script.js
@@ -0,0 +1,34 @@
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
+
+import tabs from './modules/tabs';
+import modal from './modules/modal';
+import timer from './modules/timer';
+import cards from './modules/cards';
+import calc from './modules/calc';
+import forms from './modules/forms';
+import slider from './modules/slider';
+import {openModal} from './modules/modal';
+
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 300000);
+
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    modal('[data-modal]', '.modal', modalTimerId);
+    timer('.timer', '2021-07-07');
+    cards();
+    calc();
+    forms('form', modalTimerId);
+    slider({
+        container: '.offer__slider', 
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner',
+        slide: '.offer__slide'
+    });
+});
